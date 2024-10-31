e0459323a9ae4fcc597a6be0fb9ea871b0cd6e74
mizza-cz <mizza.cz666@gmail.com>
2024-10-31 11:33:03 +0100

    add hp

---
+++ src/components/television-section/television-section.js
@@ -0,0 +1,23 @@
+$(".television-section__inner").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: false,
+  prevArrow:
+    '<button class="television__btn television__btnprev"><img src="images/ico/slider-left.svg" loading="lazy" alt="" /></button> ',
+  nextArrow:
+    ' <button class="television__btn  television__btnnext"><img src="/images/ico/slider-right.svg" loading="lazy" alt="" /></button>',
+  responsive: [
+    {
+      breakpoint: 1240,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
